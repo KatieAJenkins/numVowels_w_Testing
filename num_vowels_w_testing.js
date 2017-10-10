0c612862f8774cf3951d349d7a1bc55d1121aae9
KatieAJenkins <kind2karma@gmail.com>
2017-10-10 11:21:14 -0600

    cleaned up code

--- num_vowels_w_testing.js
+++ num_vowels_w_testing.js
@@ -1,23 +1,25 @@
-'use strict';
-//Write a function that takes a single string as the input then return the number of vowels the string contains. Do not count "y" as a vowel.
-// write tests to make sure your code works,
-
-//IN: string
-//OUT: number
-
-var string = "Mississippi";
-
-function numVowels(string) {
-//count vowels variable
-var vowelCount = 0;
-
-for (var i = 0; i <= string.length; i++){
-    if (string[i] === "a" || string[i] === "e" || string[i] === "i" || string[i] === "o" || string[i] ==="u"){
-      vowelCount += 1;
-    }
-  }
-  console.log(vowelCount);
-  return vowelCount;
-};
-
-numVowels(string);
+// 'use strict';
+// //Write a function that takes a single string as the input then return the number of vowels the string contains. Do not count "y" as a vowel.
+// // write tests to make sure your code works,
+//
+// //IN: string
+// //OUT: number
+//
+// var string = "Mississippi";
+//
+// function numVowels(string) {
+// //count vowels variable
+// var vowelCount = 0;
+//
+// for (var i = 0; i <= string.length; i++){
+//     if (string[i] === "a" || string[i] === "e" || string[i] === "i" || string[i] === "o" || string[i] ==="u"){
+//       vowelCount += 1;
+//     }
+//   }
+//   console.log(vowelCount);
+//   return vowelCount;
+// };
+//
+// numVowels(string);
+//
+// module.exports = numVowels;
--- test/vowel.test.js
+++ test/vowel.test.js
@@ -1,8 +1,22 @@
 var assert = require('assert');
-describe('Array', function() {
-  describe('#indexOf()', function() {
-    it('should return -1 when the value is not present', function() {
-      assert.equal(-1, [1,2,3].indexOf(4));
-    });
-  });
-});
+// require('chai')
+// var numVowels ='../num_vowels_w_testing.js';
+// console.log(numVowels)
+//
+// describe('numVowels', function() {
+//   describe('invoke numVowels', function() {
+//     it('should equal the number of vowels in a string', function() {
+//       let string = "arlo";
+//       let expected = numVowels(string)
+//       assert.equal(expected, numVowels(string));
+//     });
+//   });
+// });
+
+
+var expected = add(1,2);
+
+// these three assertions are equivalent:
+assert(expected == 3, 'one plus two is three');
+assert.ok(expected == 3, 'one plus two is three');
+assert.equal(expected, 3, 'one plus two is three');
